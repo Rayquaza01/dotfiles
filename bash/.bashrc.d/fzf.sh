a47aa9aecc8b957a4b1b7728862df20640ba083b
Arnaught <rayquaza01@outlook.com>
2024-08-02 00:00:51 -0400

    [bash] remove unnecessary args from fzf's fd commands

--- bash/.bashrc.d/fzf.sh
+++ bash/.bashrc.d/fzf.sh
@@ -3,6 +3,6 @@ if [[ -f "/usr/share/fzf/shell/key-bindings.bash" ]]; then
     source /usr/share/fzf/shell/key-bindings.bash
 fi
 
-export FZF_DEFAULT_COMMAND="fd -H --no-require-git ."
+export FZF_DEFAULT_COMMAND="fd -H"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd -t d -H --no-require-git . $HOME"
+export FZF_ALT_C_COMMAND="fd -t d -H . $HOME"
