ac85e935774a045d577094b138991c9bbf3872c9
Arnaught <rayquaza01@outlook.com>
2024-10-09 11:28:08 -0400

    [fish] move aliases to functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,27 +5,4 @@ if status is-interactive
     # export FZF_DEFAULT_COMMAND="fd -H"
     # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     # export FZF_ALT_C_COMMAND="fd -t d -H . $HOME"
-
-
-    # ALIAS CONFIG
-    alias vim="nvim"
-    export EDITOR="nvim"
-
-    alias ls='eza --icons'
-    alias ll='eza -l --git --icons'
-    alias la='eza -la --git --icons'
-
-    alias tree='eza --tree --icons'
-
-    alias flatpak-run='flatpak run $(flatpak list --columns=application | fzf)'
-
-    alias cat="bat"
-
-    alias neofetch="fastfetch"
-
-    alias yay="distrobox enter arch -- yay"
-    alias zeditor="distrobox enter arch -- zeditor"
-    alias yazi="distrobox enter arch -- yazi"
-
-    alias godot="flatpak run --command=godot org.godotengine.Godot"
 end
---
+++ fish/.config/fish/functions/la.fish
@@ -0,0 +1,4 @@
+function la --wraps='eza -la --git --icons' --description 'alias la=eza -la --git --icons'
+  eza -la --git --icons $argv
+        
+end
