f514702f0e191772447e04710f8b85d010166a12
Arnaught <rayquaza01@outlook.com>
2024-10-09 19:13:08 -0400

    [fish] use fzf plugin instead of builtin support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,3 @@
 if status is-interactive
-    # FZF CONFIG
-    fzf --fish | source
 
-    # export FZF_DEFAULT_COMMAND="fd -H"
-    # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    # export FZF_ALT_C_COMMAND="fd -t d -H . $HOME"
 end
