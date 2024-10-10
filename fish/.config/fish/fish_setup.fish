80421e632f6fd328bcc1ae6daffb338334d86ab5
Arnaught <rayquaza01@outlook.com>
2024-10-10 00:42:20 -0400

    [fish] clean fish setup script

--- fish/.config/fish/fish_setup.fish
+++ fish/.config/fish/fish_setup.fish
@@ -1,14 +1,8 @@
 #!/usr/bin/fish
-set -U fish_greeting
-
 if gum confirm "Run fisher update?";
     fisher update
 end
 
 if gum confirm "Configure tide?";
     tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time=No --classic_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Compact --icons='Many icons' --transient=No
-end
-
-if gum confirm "Set theme to Catppuccin Macchiato?";
-    fish_config theme save "Catppuccin Macchiato"
 end
