908d030cff272cf3c45f1795c76a42dd906a1788
Arnaught <rayquaza01@outlook.com>
2024-10-25 09:52:30 -0400

    [yazi] reorganize, document smart enter

--- yazi/.config/yazi/plugins/smart-enter.yazi/init.lua
+++ yazi/.config/yazi/plugins/smart-enter.yazi/init.lua
@@ -1,16 +1,16 @@
 return {
     entry = function()
+        -- leave visual mode to save the selection
         ya.manager_emit("escape", { visual = true })
 
         local h = cx.active.current.hovered
 
         -- if hovered is a directory, enter it
+        -- otherwise, open selected files
         if h and h.cha.is_dir then
             ya.manager_emit("enter", {})
-            return
+        else
+            ya.manager_emit("open", {})
         end
-
-        -- open selected files
-        ya.manager_emit("open", {})
     end,
 }
