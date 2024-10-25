559b821c14b824c3dc0e6e88a2038db4ab143cc3
Arnaught <rayquaza01@outlook.com>
2024-10-25 09:47:26 -0400

    [bash] if state directory doesn't exist, create it

--- bash/.bashrc.d/xdg.sh
+++ bash/.bashrc.d/xdg.sh
@@ -3,6 +3,9 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state
 export XDG_CACHE_HOME=$HOME/.cache
 
+if ! [ -d "${XDG_STATE_HOME}"/bash ]; then
+    mkdir -p "${XDG_STATE_HOME}"/bash
+fi
 export HISTFILE="${XDG_STATE_HOME}"/bash/history
 
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
