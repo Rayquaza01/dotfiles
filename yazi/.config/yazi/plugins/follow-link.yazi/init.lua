aae7220e8e03afcaac92662eb1ab0e906cb35d6a
Arnaught <rayquaza01@outlook.com>
2024-10-30 10:23:30 -0400

    [yazi] add jump to root, reveal link shortcuts

---
+++ yazi/.config/yazi/plugins/follow-link.yazi/init.lua
@@ -0,0 +1,10 @@
+-- https://github.com/sxyazi/yazi/discussions/1493
+return {
+   entry = function()
+      local h = cx.active.current.hovered
+      local original_url = h.link_to
+      if h and original_url then
+         ya.manager_emit("reveal", { original_url })
+      end
+   end,
+}
