30937a865acd8e23694344695cc1ef3c21ac075a
Arnaught <rayquaza01@outlook.com>
2024-11-15 14:52:55 -0500

    [fish] don't use builtin cd for y so directory history will work

--- fish/.config/fish/functions/y.fish
+++ fish/.config/fish/functions/y.fish
@@ -2,7 +2,7 @@ function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
+		cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
 end
