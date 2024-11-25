9fb06953db9ecfb952ebda010004a607d01a3b17
Arnaught <rayquaza01@outlook.com>
2024-11-24 21:39:32 -0500

    [install] rework install script to use dialog instead of gum

--- fish/.config/fish/fish_setup.fish
+++ fish/.config/fish/fish_setup.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/fish
-if gum confirm "Run fisher update?";
-    fisher update
+if type -q fisher;
+    git checkout HEAD -- ./fish/.config/fish/fish_plugins
+
+    if dialog --yesno "Run fisher update?" 0 0;
+        fisher update
+    end
 end
 
-if gum confirm "Configure tide?";
-    tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time=No --classic_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Compact --icons='Many icons' --transient=No
+if type -q tide;
+    if dialog --yesno "Configure tide?" 0 0;
+        tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time=No --classic_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Compact --icons='Many icons' --transient=No
+    end
 end
--- install.sh
+++ install.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
-if ! command -v gum &>/dev/null; then
-    echo "[error] gum not available"
-    echo "https://github.com/charmbracelet/gum"
+ok=0
+
+if ! command -v dialog &>/dev/null; then
+    echo "[error] dialog missing"
+    ok=1
+fi
+
+if ! command -v stow &>/dev/null; then
+    echo "[error] stow missing"
+    ok=1
+fi
+
+if [ "$ok" -ne 0 ]; then
     exit 1
 fi
 
-dotfiles=$(gum choose --header "Select dotfiles to install" --no-limit <dotfiles)
+if [ -z "$DIALOGRC" ]; then
+    export DIALOGRC=$(realpath ./dialog/.config/dialog/.dialogrc)
+fi
+
+dotfiles=$(
+    dialog \
+        --separate-output \
+        --stdout \
+        --checklist "Select dotfiles to install" \
+        0 0 0 \
+        bash "" off\
+        dialog "" off \
+        editorconfig "" off \
+        fd "" off \
+        fish "" off \
+        git "" off \
+        tmux "" off \
+        vim "" off \
+        yazi "" off \
+)
+
 for dotfile in $dotfiles; do
     stow --no-folding --adopt -t ~ -Sv "$dotfile"
 done
 
 if [[ "${dotfiles[*]}" =~ "vim" ]]; then
-    if gum confirm "Would you like to install vim plug?"; then
+    # if gum confirm "Would you like to install vim plug?"; then
+    if dialog --yesno "Would you like to install vim plug?" 0 0; then
         echo "Installing vim plug"
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
         sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -19,20 +50,26 @@ if [[ "${dotfiles[*]}" =~ "vim" ]]; then
 fi
 
 if [[ "${dotfiles[*]}" =~ "tmux" ]]; then
-    if gum confirm "Would you like to install tmux plugin manager?"; then
-        echo "Installing tmux plugin manager"
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    if command -v git &>/dev/null; then
+        if dialog --yesno "Would you like to install tmux plugin manager?" 0 0; then
+            echo "Installing tmux plugin manager"
+            git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        fi
     fi
 fi
 
 if [[ "${dotfiles[*]}" =~ "fish" ]]; then
-    if gum confirm "Would you like to install fisher?"; then
-        echo "Installing fisher"
-        fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-    fi
+    if command -v fish &>/dev/null; then
+        if dialog --yesno "Would you like to install fisher?" 0 0; then
+            echo "Installing fisher"
+            fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+        fi
 
-    if gum confirm "Would you like to run the fish setup script?"; then
-        echo "Running fish setup"
-        fish ./fish/.config/fish/fish_setup.fish
+        if dialog --yesno "Would you like to run the fish setup script?" 0 0; then
+            echo "Running fish setup"
+            fish ./fish/.config/fish/fish_setup.fish
+        fi
     fi
 fi
+
+dialog --clear
