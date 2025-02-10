d0887f8e7115aa7a8084245eb80d8035682adf79
Arnaught <rayquaza01@outlook.com>
2025-02-10 17:54:30 -0500

    [bash] fix y command

--- bash/.config/bash/functions/y.sh
+++ bash/.config/bash/functions/y.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 function y() {
-    local tmp
-	tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		builtin cd -- "$cwd" || return
