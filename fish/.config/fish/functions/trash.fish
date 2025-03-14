c775dc53eed7dc472aa7d3d250a39f84c4279973
Arnaught <rayquaza01@outlook.com>
2025-03-14 13:12:52 -0400

    [bash/fish] remove trash alias

--- fish/.config/fish/functions/trash.fish
+++
@@ -1,9 +0,0 @@
-function trash
-    if command -q trash
-        command trash -- $argv
-    else if command -q gio
-        gio trash -- $argv
-    else
-        echo "no trash command available"
-    end
-end
