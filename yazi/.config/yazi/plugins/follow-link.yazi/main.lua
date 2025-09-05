ec0c57adbb2b5eb7bbd2282132691cebd42919d3
Arnaught <rayquaza01@outlook.com>
2025-09-04 21:58:43 -0400

    [yazi] fix follow link plugin @sync comment needs to be the first line

--- yazi/.config/yazi/plugins/follow-link.yazi/main.lua
+++ yazi/.config/yazi/plugins/follow-link.yazi/main.lua
@@ -1,9 +1,11 @@
--- https://github.com/sxyazi/yazi/discussions/1493
 --- @sync entry
+-- https://github.com/sxyazi/yazi/discussions/1493
 return {
    entry = function()
+      ya.dbg("hi")
       local h = cx.active.current.hovered
       local original_url = h.link_to
+      ya.dbg(original_url)
       if h and original_url then
          ya.manager_emit("reveal", { original_url })
       end
