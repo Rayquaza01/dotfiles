8650b3482e8bd7959810341b44c819bb09dabdf3
Arnaught <rayquaza01@outlook.com>
2025-10-06 18:21:03 -0400

    [install] choice for tide prompt in fish setup

--- fish_setup.fish
+++ fish_setup.fish
@@ -8,7 +8,20 @@ if type -q fisher;
 end
 
 if type -q tide;
-    if dialog --yesno "Configure tide?" 0 0;
-        tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time=No --classic_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Compact --icons='Many icons' --transient=No
+    set tide_choice (
+        dialog \
+            --stdout \
+            --title "Tide Prompt" \
+            --menu "Option:" \
+            0 0 2 \
+            1 "Fancy" \
+            2 "Minimal"
+    )
+
+    switch "$tide_choice"
+        case 1
+            tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time=No --classic_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Compact --icons='Many icons' --transient=No
+        case 2
+            tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
     end
 end
