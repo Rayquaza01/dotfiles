1626cde8541b284e150a31d53afcedfcdac46283
Arnaught <rayquaza01@outlook.com>
2025-12-10 23:40:48 -0500

    [yazi] change ignored sign to /

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -3,6 +3,7 @@ th.git = th.git or {}
 th.git.modified_sign = "M"
 th.git.deleted_sign = "D"
 th.git.untracked_sign = "?"
+th.git.ignored_sign = "/"
 require("git"):setup()
 
 --- === STARSHIP CONFIG ===
