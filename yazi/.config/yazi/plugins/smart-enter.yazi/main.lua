f8c29341590a8b424897d525d9c472394cebc951
Arnaught <rayquaza01@outlook.com>
2026-01-30 19:03:21 -0500

    [yazi] fix plugins (again) manager_emit and mgr_emit are both deprecated in favor of emit https://github.com/sxyazi/yazi/pull/2653

--- yazi/.config/yazi/plugins/smart-enter.yazi/main.lua
+++ yazi/.config/yazi/plugins/smart-enter.yazi/main.lua
@@ -2,16 +2,16 @@
 return {
     entry = function()
         -- leave visual mode to save the selection
-        ya.mgr_emit("escape", { visual = true })
+        ya.emit("escape", { visual = true })
 
         local h = cx.active.current.hovered
 
         -- if hovered is a directory, enter it
         -- otherwise, open selected files
         if h and h.cha.is_dir then
-            ya.mgr_emit("enter", {})
+            ya.emit("enter", {})
         else
-            ya.mgr_emit("open", {})
+            ya.emit("open", {})
         end
     end,
 }
